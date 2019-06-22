1139a64c54f77dd9f7ef7e66f4b6e68b1c066b4a
listingslab <listingslab@gmail.com>
2019-06-22 10:23:48 +1000

    refactory()]

--- src/index.js
+++ src/index.js
@@ -1,18 +1,12 @@
+
 import packageJSON from "../package.json";
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 console.log(`${packageJSON.name} ${packageJSON.version} (${process.env.REACT_APP_ENV})`);
 
-
 ReactDOM.render(<App />, document.getElementById('docsify-react'));
 
-
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.register();
